ccb1e9da8547ad005405e328a0ebc104d4a672d1
cmkelling <106550296+cmkelling@users.noreply.github.com>
2022-07-05 10:57:22 -0500

    commit

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 class Card {
-    constructor(value, name, suit) {
+    constructor(name, suit, value) {
         this.value = value;
         this.name = name;
         this.suit = suit;
-    }
-}
+    };
+};
 
 class Deck {
     constructor() {
         this.cards = [];
-    }
+    };
 
     createDeck(){
         let names = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
         let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
         let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-        for (let s = 0; s < this.suits.length; s++) {
+        for (let s = 0; s < suits.length; s++) {
             for (var n = 0; n < names.length; n++) {
                 this.cards.push(new Card(names[n], suits[s], values[n]));
             };
@@ -24,28 +24,31 @@ class Deck {
 
     };
 
-    shuffleDeck(r) {
-        for (var j, x, i = r.length; i; j = parseInt(Math.random() * i), x = r[--i], r[i] = r[j], r[j] = x);
-    return r;
+    shuffleDeck() {
+        let location1, location2, tmp;
+       for (let i = 0; i < 1000; i++) {
+           location1 = Math.floor((Math.random() * this.cards.length));
+           location2 = Math.floor((Math.random() * this.cards.length));
+           tmp = this.cards[location1];
+           this.cards[location1] = this.cards[location2];
+           this.cards[location2] = tmp;
+       };
 
     };
-}
-
-const myDeck = new Deck();
-console.log(myDeck);
-
+};
+var myDeck = new Deck();
 class Player {
     constructor(name) {
         this.playerName = name;
         this.playerCards = [];
-    }
-}
+    };
+};
 
 class Board {
     constructor() {
         this.players = [];
         this.cardsInMiddle = [];
-    }
+    };
 
     start(playerOne, playerTwo) {
         this.players.push(new Player(playerOne));
@@ -55,9 +58,10 @@ class Board {
         myDeck.shuffleDeck();
         this.players[0].playerCards = myDeck.cards.slice(0, 26);
         this.players[1].playerCards = myDeck.cards.slice(26, 52);
-    }
-}
+    };
+};
 
 let gameBoard = new Board;
 gameBoard.start('one', 'two');
 console.log(gameBoard.players);
+console.log(myDeck);
